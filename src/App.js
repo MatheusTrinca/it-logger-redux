28350d5728954bfc61e060d29a697033e9245597
Nalu e Matheus <NaluMatheus@MacBook-de-Nalu.local>
2021-04-11 20:35:25 -0300

    search, logs e logitem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import Searchbar from './components/layout/Searchbar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
+    // Init MaterializeCSS
     M.AutoInit();
   });
-  return <div className="App"></div>;
+  return (
+    <Fragment>
+      <Searchbar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
